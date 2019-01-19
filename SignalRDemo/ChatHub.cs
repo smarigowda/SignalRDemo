0ecdf5f4b61b142ccc37fd305c9f1877727bcbb9
Santosh Marigowda <santosharakere@gmail.com>
2019-01-19 11:32:21 +0000

    adding console logs

--- SignalRDemo/ChatHub.cs
+++ SignalRDemo/ChatHub.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using System;
 
 namespace SignalRChat.Hubs
 {
@@ -7,6 +8,8 @@ namespace SignalRChat.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
+            Console.WriteLine("--------------- ----------------------------" + user);
+            Console.WriteLine("--------------- ----------------------------" + message);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
